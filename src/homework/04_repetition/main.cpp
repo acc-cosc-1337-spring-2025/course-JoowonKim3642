6ef439b74bbfc24f9a08ae1f40bcd4c1cce667d8
JoowonKim3642 <joowon.kim@g.austincc.edu>
2025-03-02 06:05:07 +0000

    hw4

--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,8 +1,59 @@
 //write include statements
-
+#include <iostream>
+#include "repetition.h"
 //write using statements
+using std::cin;
+using std::cout;
+
+void usefactorial() 
+{
+	int num;
+	cout << "number: ";
+	cin >> num;
+	cout << "factorial " << factorial(num) << "\n";
+}
+void usegcd() 
+{
+	int num1, num2;
+	cout << "Enter first number: ";
+	cin >> num1;
+	cout << "Enter second number: ";
+	cin >> num2;
+	cout << "gcd: " << gcd(num1,num2) << "\n";
+}
 
 int main() 
 {
+	int choice;
+    do {
+        cout << "\nMenu:\n";
+        cout << "1 - Factorial\n";
+        cout << "2 - Greatest Common Divisor\n";
+        cout << "3 - Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        if (choice == 1) {
+            usefactorial();
+        } 
+
+        else if (choice == 2) {
+            usegcd();
+        } 
+
+        else if (choice == 3) {
+            char confirm;
+            cout << "Are you sure you want to exit? (y/n): ";
+            cin >> confirm;
+            if (confirm == 'y' || confirm == 'Y') {
+                break;
+            }
+        } 
+
+        else {
+            cout << "Invalid choice. Please try again.\n";
+        }
+    } while (true);
+
 	return 0;
 }
--- src/homework/04_repetition/repetition.cpp
+++ src/homework/04_repetition/repetition.cpp
@@ -1,3 +1,30 @@
 //add include statements
-
+#include "repetition.h"
 //add function(s) code here
+
+int factorial(int num) {
+    int result = 1;
+    for (int i = 1; i <= num; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+int gcd(int num1,int num2)
+{
+    int x;
+    while(num1 != num2)
+    {
+        if(num1<num2)
+        {
+            x = num1;
+            num1 = num2;
+            num2 = x;
+        }
+        if(num1>num2)
+        {
+            num1 -= num2;
+        }
+    }
+    return num1;
+}
